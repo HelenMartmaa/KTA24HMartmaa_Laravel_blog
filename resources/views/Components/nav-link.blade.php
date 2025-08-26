114abe75ccc4ada99f6772ff7ee223421ff7d8c5
Helen Martmaa <helen.aitai@gmail.com>
2025-08-26 09:40:34 +0300

    Route wildcards, update About page to Job listings page

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,17 +1,5 @@
 @props(['active' => false])
 
-@props(['type' => 'a'])
-
-@if($type === 'a')
-    <a {{ $attributes->merge(['class' => 'text-blue-500']) }}>
-        {{ $slot }}
-    </a>
-@elseif($type === 'button')
-    <button {{ $attributes->merge(['class' => 'bg-blue-500 text-white px-3 py-1 rounded']) }}>
-        {{ $slot }}
-    </button>
-@endif
-
 <a class="{{ $active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium"
     aria-current="{{ $active ? 'page': 'false' }}"
     {{ $attributes }}
